2192104230e58fdfba6f64a2728ed52f0488d340
Nikeplusruss <nikeplusruss@icloud.com>
2018-02-07 18:37:43 -0800

    Update Variables to new Server

--- auth0-variables.js
+++ auth0-variables.js
@@ -1,7 +1,3 @@
 var AUTH0_CLIENT_ID='P4Td6xntQjYiN4mO6iKt50kgpWb19EDf';
 var AUTH0_DOMAIN='pride.auth0.com';
-<<<<<<< HEAD
 var AUTH0_CALLBACK_URL='http://138.68.235.38:3000';
-=======
-var AUTH0_CALLBACK_URL='https://nikeplusruss.herokuapp.com';
->>>>>>> bbc4e3e1a46373d56cf08231e6571d53d350a161
