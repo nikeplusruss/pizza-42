7bb7f4c00403d7949edfcb1ab83579b9e578e943
Nikeplusruss <nikeplusruss@icloud.com>
2018-05-02 16:24:00 -0700

    Remove Doocker Files and New Backend API using Node + Express

--- app.js
+++ app.js
@@ -8,32 +8,53 @@ $('document').ready(function() {
     domain: AUTH0_DOMAIN,
     clientID: AUTH0_CLIENT_ID,
     redirectUri: AUTH0_CALLBACK_URL,
-    audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
+    // Set the Identifier for our new API
+    audience: 'http://pizza-app.com/api',
     responseType: 'token id_token',
-    scope: 'openid profile read:users',
+    // Scope - Limit User to Create and Read - No Delete
+    scope: 'openid profile create:pizza_orders read:pizza_orders',
     leeway: 60
   });
 
+  // Create variables for DOM Sections
   var loginStatus = $('.container h4');
   var loginView = $('#login-view');
   var homeView = $('#home-view');
+  var profileView = $('#profile-view');
+  var pingView = $('#ping-view');
 
   // buttons and event listeners
   var homeViewBtn = $('#btn-home-view');
   var loginBtn = $('#btn-login');
   var logoutBtn = $('#btn-logout');
+  var orderBtn = $('#btn-order');
 
+  // Profile View
+  var userProfile;
+
+  // Home Button Click
   homeViewBtn.click(function() {
     homeView.css('display', 'inline-block');
     loginView.css('display', 'none');
   });
 
+  // Login Button Click
   loginBtn.click(function(e) {
     e.preventDefault();
     webAuth.authorize();
   });
 
+  // Order Button Click
+  orderBtn.click(function(e) {
+    e.preventDefault();
+    console.log('Prepare for Pizza Order Goodness');
+    // Call the API with Scope Authorization
+    callAPI('/private-scoped');
+  });
+
+  // Logout Button Click
   logoutBtn.click(logout);
+
 
   function setSession(authResult) {
     // Set the time that the access token will expire at
@@ -67,6 +88,7 @@ $('document').ready(function() {
         setSession(authResult);
         loginBtn.css('display', 'none');
         homeView.css('display', 'inline-block');
+        // Let's view our User Data in the Console
         console.log(authResult);
       } else if (err) {
         homeView.css('display', 'inline-block');
@@ -84,12 +106,69 @@ $('document').ready(function() {
       loginBtn.css('display', 'none');
       logoutBtn.css('display', 'inline-block');
       loginStatus.text('Congratulations, you are logged in, and will be able to place an order once this company actually exists! =)');
+      // Display the Profile
+      getProfile();
     } else {
       loginBtn.css('display', 'inline-block');
       logoutBtn.css('display', 'none');
-      loginStatus.text('You are not logged in! Please log in to continue.');
+      loginStatus.text('You are not logged in! Please log in to Order Pizza.');
+      profileView.css('display', 'none');
+      pingView.css('display', 'none');
     }
   }
 
+  function getProfile() {
+    if (!userProfile) {
+      var accessToken = localStorage.getItem('access_token');
+
+      if (!accessToken) {
+        console.log('Access Token must exist to fetch profile');
+      }
+
+      webAuth.client.userInfo(accessToken, function(err, profile) {
+        if (profile) {
+          userProfile = profile;
+          displayProfile();
+        }
+      });
+    } else {
+      displayProfile();
+    }
+  }
+
+  function displayProfile() {
+    // display the profile
+    $('#profile-view .nickname').text(userProfile.nickname);
+    $('#profile-view .full-profile').text(JSON.stringify(userProfile, null, 2));
+    $('#profile-view img').attr('src', userProfile.picture);
+  }
+
+  // Let's Communicate with the API
+
+  var apiUrl = 'http://localhost:3010/api';
+
+  // ...
+  function callAPI(endpoint) {
+    var url = apiUrl + endpoint;
+    var accessToken = localStorage.getItem('access_token');
+
+    var headers;
+    if (accessToken) {
+      headers = { Authorization: 'Bearer ' + accessToken };
+    }
+
+    $.ajax({
+      url: url,
+      headers: headers
+    })
+      .done(function(result) {
+        $('#ping-view h2').text(result.message);
+      })
+      .fail(function(err) {
+        $('#ping-view h2').text('Request failed: ' + err.statusText);
+      });
+  }  
+
+  // Authienticate
   handleAuthentication();
 });
--- auth0-variables.js
+++ auth0-variables.js
@@ -1,3 +1,6 @@
 var AUTH0_CLIENT_ID='P4Td6xntQjYiN4mO6iKt50kgpWb19EDf';
 var AUTH0_DOMAIN='pride.auth0.com';
-var AUTH0_CALLBACK_URL='http://165.227.55.10:3000';
+// Local Dev
+var AUTH0_CALLBACK_URL='http://localhost:3000';
+// Public IP
+// var AUTH0_CALLBACK_URL='http://165.227.55.10:3000';
