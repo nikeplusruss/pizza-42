100150c6ee24b975e5f59dde2cb3d0fe7ea3556e
Nikeplusruss <nikeplusruss@icloud.com>
2018-05-03 04:53:13 +0000

    updated server variables

--- auth0-variables.js
+++ auth0-variables.js
@@ -1,6 +1,6 @@
 var AUTH0_CLIENT_ID='P4Td6xntQjYiN4mO6iKt50kgpWb19EDf';
 var AUTH0_DOMAIN='pride.auth0.com';
 // Local Dev
-var AUTH0_CALLBACK_URL='http://localhost:3000';
+//var AUTH0_CALLBACK_URL='http://localhost:3000';
 // Public IP
-// var AUTH0_CALLBACK_URL='http://165.227.55.10:3000';
+var AUTH0_CALLBACK_URL='http://165.227.55.10:3000';
